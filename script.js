9a048a20d4df7ac67e9a6c5b869246c3e081ee22
Andres <ballerlikemike23@gmail.com>
2018-06-28 21:58:35 -0400

    Arrow Functions Lexical this Keyword

--- script.js
+++ script.js
@@ -119,3 +119,77 @@ ages6 = years.map((el, index) => {
 })
 
 console.log(ages6)
+
+//////////////////////////////////////////
+// Lecture: Arrow Functions 2
+
+// ES5
+var box5 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+
+    var self = this
+    document.querySelector('.green').addEventListener('click', function() {
+      var str = 'This is box number ' + self.position + ' and it is ' + self.color
+      alert(str)
+    })
+  }
+}
+// box5.clickMe()
+
+// ES6
+const box6 = {
+  color: 'green',
+  position: 1,
+  clickMe: function() {
+    document.querySelector('.green').addEventListener('click', () => { // arrow functions gives access to this keyword without self hack
+      let str = 'This is box number ' + this.position + ' and it is ' + this.color
+      alert(str)
+    })
+  }
+}
+// box6.clickMe()
+
+/*
+const box66 = {
+  color: 'green',
+  position: 1,
+  clickMe: () => { // would share the global this which is undefined
+    document.querySelector('.green').addEventListener('click', () => { // arrow functions gives access to this keyword without self hack
+      let str = 'This is box number ' + this.position + ' and it is ' + this.color
+      alert(str)
+    })
+  }
+}
+box66.clickMe()
+*/
+
+function Person(name) {
+  this.name = name
+}
+
+// ES5
+Person.prototype.myFriends5 = function(friends) {
+
+  var arr = friends.map(function(el) {
+    return this.name + ' is friends with ' + el
+  }.bind(this))
+
+  console.log(arr)
+}
+
+var friends = ['Bob', 'Jane', 'Mark']
+new Person('John').myFriends5(friends)
+
+// ES6
+Person.prototype.myFriends6 = function(friends) {
+
+  let arr = friends.map((el) => {
+    return `${this.name} is friends with ${el}`
+  })
+
+  console.log(arr)
+}
+
+new Person('Mike').myFriends6(friends)
