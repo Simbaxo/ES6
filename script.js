6a0c4a7182b1371662138ab439101e39bd194d29
Andres <ballerlikemike23@gmail.com>
2018-06-30 00:24:46 -0400

    Maps

--- script.js
+++ script.js
@@ -352,3 +352,28 @@ isFullAge6(1990, 2001, 1965)
  var emaily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish')
 
  // ES6 Define parameters in the function call
+
+//////////////////////////////////////////
+// Lecture: Maps
+
+const question = new Map()
+question.set('question', 'What is the official name of the latest major JavaScript version?')
+question.set(1, 'ES5')
+question.set(2, 'ES6')
+question.set(3, 'ES2015')
+question.set(4, 'ES7')
+question.set('correct', 3)
+question.set(true, 'Correct answer :D')
+question.set(false, 'Wrong, please try again!')
+
+console.log(question.get('question'))
+console.log(question.size)
+
+if(question.has(4)) {
+  // question.delete(4)
+  console.log('Answer 4 is here')
+}
+
+// question.clear()
+
+question.forEach((value, key) => console.log(`This is ${key}, and its set to ${value}`))
