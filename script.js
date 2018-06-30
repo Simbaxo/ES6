acd5d810eff9c5e184f78e69e29ae7e96866ae93
Andres <ballerlikemike23@gmail.com>
2018-06-30 10:11:32 -0400

    Classes

--- script.js
+++ script.js
@@ -377,3 +377,37 @@ if(question.has(4)) {
 // question.clear()
 
 question.forEach((value, key) => console.log(`This is ${key}, and its set to ${value}`))
+
+//////////////////////////////////////////
+// Lecture: Classes
+
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name
+  this.yearOfBirth = yearOfBirth
+  this.job = job
+}
+
+Person5.prototype.calculateAge = function() {
+  var age = new Date().getFullYear - this.yearOfBirth
+  console.log(age)
+}
+
+var john5 = new Person5('John', 1990, 'teacher')
+console.log(john5)
+
+// ES6
+class Person6 {
+  constructor (name, yearOfBirth, job) {
+    this.name = name
+    this.yearOfBirth = yearOfBirth
+    this.job = job
+  }
+
+  calculateAge() {
+    let age = new Date().getFullYear - this.yearOfBirth
+    console.log(age)
+  }
+}
+
+const john6 = new Person6('John', 1990, 'teacher')
