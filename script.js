f4a8d75dd31476b36297bffcf075f2f6fcc627cf
Andres <ballerlikemike23@gmail.com>
2018-06-30 10:38:43 -0400

    Classes with Subclasses

--- script.js
+++ script.js
@@ -381,6 +381,7 @@ question.forEach((value, key) => console.log(`This is ${key}, and its set to ${v
 //////////////////////////////////////////
 // Lecture: Classes
 
+/*
 // ES5
 var Person5 = function(name, yearOfBirth, job) {
   this.name = name
@@ -409,5 +410,70 @@ class Person6 {
     console.log(age)
   }
 }
+*/
 
-const john6 = new Person6('John', 1990, 'teacher')
+// const john6 = new Person6('John', 1990, 'teacher')
+
+//////////////////////////////////////////
+// Lecture: Classes with Subclasses
+
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name
+  this.yearOfBirth = yearOfBirth
+  this.job = job
+}
+
+Person5.prototype.calculateAge = function() {
+  var age = new Date().getFullYear() - this.yearOfBirth
+  console.log(age)
+}
+
+var Athlete5 = function(name, yearOfBirth, job, olymicGames, medals) {
+  Person5.call(this, name, yearOfBirth, job)
+  this.olymicGames = olymicGames
+  this.medals = medals
+}
+
+Athlete5.prototype = Object.create(Person5.prototype)
+
+Athlete5.prototype.wonMedal = function() {
+  this.medals++
+  console.log(this.medals)
+}
+var johnAthlete5 = new Athlete5('John', 1990, 'swimmer', 3, 10)
+
+johnAthlete5.calculateAge()
+johnAthlete5.wonMedal()
+
+// ES6
+class Person6 {
+  constructor (name, yearOfBirth, job) {
+    this.name = name
+    this.yearOfBirth = yearOfBirth
+    this.job = job
+  }
+
+  calculateAge() {
+    let age = new Date().getFullYear() - this.yearOfBirth
+    console.log(age)
+  }
+}
+
+class Athlete6 extends Person6 {
+  constructor(name, yearOfBirth, job, olympicGames, medals) {
+    super(name, yearOfBirth, job)
+    this.olympicGames = olympicGames
+    this.medals = medals
+  } 
+
+  wonMedal() {
+    this.medals++
+    console.log(this.medals)
+  }
+}
+
+const johnAthlete6 = new Athlete6('John', 1990, 'swimmer', 3, 10)
+
+johnAthlete6.wonMedal()
+johnAthlete6.calculateAge()
